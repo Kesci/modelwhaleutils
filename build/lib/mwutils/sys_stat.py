2da2357309ef5bd0df3c2bebf0ab93bdca554866
gerardtao <gerardtao0315@kesci.com>
2023-06-29 16:57:35 +0800

    linkmlflow

--- build/lib/mwutils/sys_stat.py
+++ build/lib/mwutils/sys_stat.py
@@ -87,7 +87,9 @@ class SystemStats(object):
                     self.sampler[stat] = self.sampler.get(stat, [])
                     self.sampler[stat].append(value)
             self.samples += 1
+            print('sample ++')
             if self._shutdown or self.samples >= self.samples_to_average:
+                print('flush')
                 self.flush()
                 if self._shutdown:
                     break
@@ -99,6 +101,7 @@ class SystemStats(object):
                     break
 
     def shutdown(self):
+        print('SHUTDOWN')
         self._shutdown = True
         try:
             self._thread.join()
